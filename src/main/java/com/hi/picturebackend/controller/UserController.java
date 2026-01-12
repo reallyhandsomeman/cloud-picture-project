6a3f38031cba5f4de32127bc02127e79a69f156b
zzl <zonglinzou@qq.com>
2026-01-12 16:20:46 +0800

    用户注册数据模型

---
+++ src/main/java/com/hi/picturebackend/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.hi.picturebackend.controller;
+
+import com.hi.picturebackend.common.BaseResponse;
+import com.hi.picturebackend.common.ResultUtils;
+import com.hi.picturebackend.exception.ErrorCode;
+import com.hi.picturebackend.exception.ThrowUtils;
+import com.hi.picturebackend.model.dto.user.UserRegisterRequest;
+import com.hi.picturebackend.service.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Resource
+    private UserService userService;
+
+    /**
+     * 用户注册
+     */
+    @PostMapping("/register")
+    public BaseResponse<Long> userRegister(@RequestBody UserRegisterRequest userRegisterRequest) {
+        ThrowUtils.throwIf(userRegisterRequest == null, ErrorCode.PARAMS_ERROR);
+        String userAccount = userRegisterRequest.getUserAccount();
+        String userPassword = userRegisterRequest.getUserPassword();
+        String checkPassword = userRegisterRequest.getCheckPassword();
+        long result = userService.userRegister(userAccount, userPassword, checkPassword);
+        return ResultUtils.success(result);
+    }
+}
+
+
+
