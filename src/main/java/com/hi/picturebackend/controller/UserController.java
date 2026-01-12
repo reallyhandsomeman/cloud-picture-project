6b7e10d6ab09d632e1595a8efbf3e981505bb437
zzl <zonglinzou@qq.com>
2026-01-12 17:00:04 +0800

    用户登陆的service和impl实现

--- src/main/java/com/hi/picturebackend/controller/UserController.java
+++ src/main/java/com/hi/picturebackend/controller/UserController.java
@@ -4,14 +4,15 @@ import com.hi.picturebackend.common.BaseResponse;
 import com.hi.picturebackend.common.ResultUtils;
 import com.hi.picturebackend.exception.ErrorCode;
 import com.hi.picturebackend.exception.ThrowUtils;
+import com.hi.picturebackend.model.dto.user.UserLoginRequest;
 import com.hi.picturebackend.model.dto.user.UserRegisterRequest;
+import com.hi.picturebackend.model.entity.User;
+import com.hi.picturebackend.model.vo.LoginUserVO;
 import com.hi.picturebackend.service.UserService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping("/user")
@@ -31,6 +32,22 @@ public class UserController {
         String checkPassword = userRegisterRequest.getCheckPassword();
         long result = userService.userRegister(userAccount, userPassword, checkPassword);
         return ResultUtils.success(result);
+    }
+
+    @PostMapping("/login")
+    public BaseResponse<LoginUserVO> userLogin(@RequestBody UserLoginRequest userLoginRequest, HttpServletRequest request) {
+        ThrowUtils.throwIf(userLoginRequest == null, ErrorCode.PARAMS_ERROR);
+        String userAccount = userLoginRequest.getUserAccount();
+        String userPassword = userLoginRequest.getUserPassword();
+        LoginUserVO loginUserVO = userService.userLogin(userAccount, userPassword, request);
+        return ResultUtils.success(loginUserVO);
+    }
+
+    @GetMapping("/get/login")
+    public BaseResponse<LoginUserVO> getLoginUser(HttpServletRequest request) {
+        User user = userService.getLoginUser(request);
+        // 返回脱敏数据LoginUserVO
+        return ResultUtils.success(userService.getLoginUserVO(user));
     }
 }
 
