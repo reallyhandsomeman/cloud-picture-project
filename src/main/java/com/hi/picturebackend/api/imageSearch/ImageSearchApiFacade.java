498eb45368d68a2c101835f1ab75cb97ce8a5148
zzl <zonglinzou@qq.com>
2026-01-29 13:30:51 +0800

    以图搜图API

---
+++ src/main/java/com/hi/picturebackend/api/imageSearch/ImageSearchApiFacade.java
@@ -0,0 +1,33 @@
+package com.hi.picturebackend.api.imageSearch;
+
+import com.hi.picturebackend.api.imageSearch.model.ImageSearchResult;
+import com.hi.picturebackend.api.imageSearch.sub.GetImageFirstUrlApi;
+import com.hi.picturebackend.api.imageSearch.sub.GetImageListApi;
+import com.hi.picturebackend.api.imageSearch.sub.GetImagePageUrlApi;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+@Slf4j
+public class ImageSearchApiFacade {
+
+    /**
+     * 搜索图片
+     *
+     * @param imageUrl
+     * @return
+     */
+    public static List<ImageSearchResult> searchImage(String imageUrl) {
+        String imagePageUrl = GetImagePageUrlApi.getImagePageUrl(imageUrl);
+        String imageFirstUrl = GetImageFirstUrlApi.getImageFirstUrl(imagePageUrl);
+        List<ImageSearchResult> imageList = GetImageListApi.getImageList(imageFirstUrl);
+        return imageList;
+    }
+
+    public static void main(String[] args) {
+        // 测试以图搜图功能
+        String imageUrl = "https://www.codefather.cn/logo.png";
+        List<ImageSearchResult> resultList = searchImage(imageUrl);
+        System.out.println("结果列表" + resultList);
+    }
+}
