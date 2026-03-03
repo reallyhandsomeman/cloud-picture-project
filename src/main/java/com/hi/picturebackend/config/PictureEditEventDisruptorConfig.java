b68a0ffc4d82f485c9f6bdcf7fa0046647e6209b
zzl <zonglinzou@qq.com>
2026-03-03 14:15:51 +0800

    引入disruptor

---
+++ src/main/java/com/hi/picturebackend/config/PictureEditEventDisruptorConfig.java
@@ -0,0 +1,33 @@
+package com.hi.picturebackend.config;
+
+import cn.hutool.core.thread.ThreadFactoryBuilder;
+import com.hi.picturebackend.manager.websocket.disruptor.PictureEditEvent;
+import com.hi.picturebackend.manager.websocket.disruptor.PictureEditEventWorkHandler;
+import com.lmax.disruptor.dsl.Disruptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class PictureEditEventDisruptorConfig {
+
+    @Resource
+    private PictureEditEventWorkHandler pictureEditEventWorkHandler;
+
+    @Bean("pictureEditEventDisruptor")
+    public Disruptor<PictureEditEvent> messageModelRingBuffer() {
+        // ringBuffer 的大小
+        int bufferSize = 1024 * 256;
+        Disruptor<PictureEditEvent> disruptor = new Disruptor<>(
+                PictureEditEvent::new,
+                bufferSize,
+                ThreadFactoryBuilder.create().setNamePrefix("pictureEditEventDisruptor").build()
+        );
+        // 设置消费者
+        disruptor.handleEventsWithWorkerPool(pictureEditEventWorkHandler);
+        // 开启 disruptor
+        disruptor.start();
+        return disruptor;
+    }
+}
---
+++ src/main/java/com/hi/picturebackend/manager/websocket/disruptor/PictureEditEvent.java
@@ -0,0 +1,31 @@
+package com.hi.picturebackend.manager.websocket.disruptor;
+
+import com.hi.picturebackend.manager.websocket.model.PictureEditRequestMessage;
+import com.hi.picturebackend.model.entity.User;
+import lombok.Data;
+import org.springframework.web.socket.WebSocketSession;
+
+@Data
+public class PictureEditEvent {
+
+    /**
+     * 消息
+     */
+    private PictureEditRequestMessage pictureEditRequestMessage;
+
+    /**
+     * 当前用户的 session
+     */
+    private WebSocketSession session;
+
+    /**
+     * 当前用户
+     */
+    private User user;
+
+    /**
+     * 图片 id
+     */
+    private Long pictureId;
+
+}
